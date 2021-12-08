ef829dcd82e9763d99e077d7c98b6f1f4af439f3
coelhovinicius <coelhovinicius@yahoo.com.br>
2021-12-08 17:14:33 -0300

    Excecoes Personalizadas - Parte 1

---
+++ Aula143_ExcecoesPersonalizadas/Entities/Booking.cs
@@ -0,0 +1,42 @@
+﻿/* >>> CLASSE BOOKING <<< */
+using System;
+namespace Aula143_ExcecoesPersonalizadas.Entities
+{
+    class Booking
+    {
+        public int RoomNumber { get;  set; }
+        public DateTime CheckIn { get;  set; }
+        public DateTime CheckOut { get;  set; }
+
+        public Booking()
+        {
+        }
+
+        public Booking(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays; // Casting do double TotalDays para inteiro
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber
+                + ", check-in: " + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: " + CheckOut.ToString("dd/MM/yyyy")
+                + ", " + Duration() + " nights";
+        }
+    }
+}
---
+++ Aula143_ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,63 @@
+﻿/* EXCECOES PERSONALIZADAS
+    
+    Exemplo: Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data de entrada e data de saída)
+    e mostrar os dados da reserva, inclusive sua duração em dias. Em seguida, ler novas datas de entrada e saída, atualizar
+    a reserva, e mostrar novamente a reserva com os dados atualizados. O programa não deve aceitar dados inválidos para a
+    reserva, conforme as seguintes regras:
+    - Alterações de reserva só podem ocorrer para datas futuras
+    - A data de saída deve ser maior que a data de entrada
+ */
+
+/* >>> PROGRAMA PRINCIPAL <<< */
+using System;
+using Aula143_ExcecoesPersonalizadas.Entities;
+
+namespace Aula143_ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Booking error: Check-in date must be earlier than check-out date");
+            }
+            else
+            {
+                Booking booking = new Booking(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Booking: " + booking);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter new dates for rebooking: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Rebooking error: Dates must be later than today");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Booking error: Check-out date must be later than check-in date");
+                }
+                else
+                {
+                    booking.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Booking: " + booking);
+                }
+            }
+        }
+    }
+}
+
+
